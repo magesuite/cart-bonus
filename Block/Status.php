7d6ca7f3398f13335e647fc3dab57f8a7fdd6606
Piotr Matras <piotr.matras@creativestyle.pl>
2024-09-13 12:52:10 +0000

    feat: FZ-334 exclude virtual product from calculation

--- Block/Status.php
+++ Block/Status.php
@@ -4,36 +4,48 @@ namespace MageSuite\CartBonus\Block;
 
 class Status extends \Magento\Framework\View\Element\Template
 {
-
     protected $_template = 'MageSuite_CartBonus::status.phtml';
 
-    /**
-     * @var \Magento\Checkout\Model\Cart
-     */
-    protected $cart;
-
-    /**
-     * @var \MageSuite\CartBonus\Service\StatusBuilder
-     */
-    protected $statusBuilder;
+    protected \Magento\Checkout\Model\Cart $cart;
+    protected \MageSuite\CartBonus\Service\StatusBuilder $statusBuilder;
+    protected \MageSuite\CartBonus\Helper\Configuration $configuration;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Checkout\Model\Cart $cart,
         \MageSuite\CartBonus\Service\StatusBuilder $statusBuilder,
+        \MageSuite\CartBonus\Helper\Configuration $configuration,
         array $data = []
-    )
-    {
+    ) {
         parent::__construct($context, $data);
         $this->cart = $cart;
         $this->statusBuilder = $statusBuilder;
+        $this->configuration = $configuration;
     }
 
     public function getBonusesStatus()
     {
-        $totals = $this->cart->getQuote()->getTotals();
-        $currentCartValue = $totals['subtotal']['value'];
+        return $this->statusBuilder->build($this->getCartValue());
+    }
 
-        return $this->statusBuilder->build($currentCartValue);
+    public function getCartValue(): float
+    {
+        $quote = $this->cart->getQuote();
+        $totals = $quote->getTotals();
+        $subtotal = $totals['subtotal']['value'];
+
+        if (!$this->configuration->isVirtualProductExcludedFromCalculation($quote->getStoreId())) {
+            return $subtotal;
+        }
+
+        foreach ($quote->getAllItems() as $item) {
+            if (!$item->getIsVirtual()) {
+                continue;
+            }
+
+            $subtotal -= $item->getRowTotalInclTax();
+        }
+
+        return $subtotal;
     }
 }
